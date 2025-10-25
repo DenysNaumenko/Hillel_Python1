8ff6a4a33dd04a998580e7f357295eba0be2d3d1
Den <denysnaumenko1@gmail.com>
2025-10-26 06:31:08 +1300

    Homework 2_1!

--- main.py
+++ main.py
@@ -1,3 +1,42 @@
-print('Hello world')
-
-#test new
+# print('Hello world')
+#
+# #test new
+# age=35
+# print(id(age))
+#
+# print(id(257))
+#
+# my_name = "Den"
+# print(my_name)
+# print(id(my_name))
+#
+# x = 10
+# y = 20
+# x, y = y, x
+# print(x, y)
+#
+# print(type(my_name))
+#
+#
+# print(115 // 3)
+# print(118 % 5) #23*5
+#
+# #divmod
+#
+# x, y = divmod(11, 3)
+# print(x, y)
+#
+# print(-11 // 3) # в меньшую сторону
+# print(-11 % 3)
+# divmod(-11, 3)
+# print(-13 // 3)
+# print(-13 % 3)
+# a = 5
+# b = 10
+# c = a + b * 3
+#
+#
+#
+# print(7254 % 1000)
+# print(7254 // 10)
+print(523 % 10)
