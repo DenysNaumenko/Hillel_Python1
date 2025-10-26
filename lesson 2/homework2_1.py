5d510d1499bd50d097008fd84c215198b8eb10af
Den <denysnaumenko1@gmail.com>
2025-10-27 00:13:21 +1300

    Homeworks 2_1 / 2_2

--- lesson 2/homework2_1.py
+++ lesson 2/homework2_1.py
@@ -1,13 +1,13 @@
 
 #Homework 2.1
-# user_input = input("Please enter a 4-digit integer number: ")
-#
-# number = int(user_input)
-# print(number // 1000)
-# print((number // 100) % 10)
-# print((number // 10) % 10)
-# print(type(number % 10))
-#
+user_input = input("Please enter a 4-digit integer number: ")
+
+number = int(user_input)
+print(number // 1000)
+print((number // 100) % 10)
+print((number // 10) % 10)
+print((number % 10))
+
 
 
 # Homework 2.2
