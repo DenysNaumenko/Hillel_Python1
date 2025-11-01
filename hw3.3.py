bb20fa364d95aa223089dc922d714aefa9f1631c
Den <denysnaumenko1@gmail.com>
2025-11-02 08:33:08 +1300

    Homework 3_3 changes

--- hw3.3.py
+++ hw3.3.py
@@ -1,12 +1,10 @@
 #lst = [1, 2, 3, 4, 5, 6] #[[1, 2, 3], [4, 5, 6]]
-#lst = [1, 2, 3] # [[1, 2], [3]]
+lst = [1, 2, 3] # [[1, 2], [3]]
 #lst = [1, 2, 3, 4, 5] # [[1, 2, 3], [4, 5]]
 #lst = [1] # [[1], []]
-lst = [] #[[], []]
+#lst = [] #[[], []]
 
-if len(lst) <= 3:
-    result = [lst[:2], lst[2:]]
-    print(result)
-else:
-    result = [lst[:3], lst[3:]]
-    print(result)
+
+
+half_part = (len(lst) + 1) // 2
+print([lst[:half_part], lst[half_part:]])
