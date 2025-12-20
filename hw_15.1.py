5d51a7b0970e4ad451049078fab907e38b001f85
Den <denysnaumenko1@gmail.com>
2025-12-21 09:01:59 +1300

    hw 15.1

---
+++ hw_15.1.py
@@ -0,0 +1,59 @@
+import math
+
+
+class Rectangle:
+
+    def __init__(self, width, height):
+        self.width = width
+        self.height = height
+
+    def get_square(self):
+        return self.width * self.height
+
+    def __eq__(self, other):
+        if not isinstance(other, Rectangle):
+            return NotImplemented
+        return self.get_square() == other.get_square()
+
+    def __add__(self, other):
+        if not isinstance(other, Rectangle):
+            return NotImplemented
+
+        total_square = self.get_square() + other.get_square()
+
+        width = int(math.sqrt(total_square))
+        while total_square % width != 0:
+            width -= 1
+
+        height = total_square // width
+        return Rectangle(width, height)
+
+    def __mul__(self, n):
+        if not isinstance(n, int):
+            return NotImplemented
+
+        new_square = self.get_square() * n
+
+        width = int(math.sqrt(new_square))
+        while new_square % width != 0:
+            width -= 1
+
+        height = new_square // width
+        return Rectangle(width, height)
+
+    def __str__(self):
+        return f"Rectangle({self.width}, {self.height})"
+
+
+r1 = Rectangle(2, 4)
+r2 = Rectangle(3, 6)
+assert r1.get_square() == 8, "Test1"
+assert r2.get_square() == 18, "Test2"
+
+r3 = r1 + r2
+assert r3.get_square() == 26, "Test3"
+
+r4 = r1 * 4
+assert r4.get_square() == 32, "Test4"
+
+assert Rectangle(3, 6) == Rectangle(2, 9), "Test5"
